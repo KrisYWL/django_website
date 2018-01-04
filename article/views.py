be96f23b0c0e9f7457a06fa2a309b597cbb5b9a8
yangwanli <1310834307@qq.com>
2018-01-04 16:22:52 +0800

    18.1.4 16:22

--- article/views.py
+++ article/views.py
@@ -23,3 +23,5 @@ def archives(request):
 	except Article.DoesNotExist:
 		raise Http404
 	return render(request,'archives.html',{'post_list':post_list,'error':False})
+def about_me(request):
+	return render(request,'aboutme.html')
--- my_blog/urls.py
+++ my_blog/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     url(r'^$',views.home),
     url(r'^(?P<id>\d+)/$',views.detail,name='detail'),
     url(r'^archives/$',views.archives,name='archives'),
+    url(r'^aboutme/$',views.about_me,name='about_me'),
 ]
